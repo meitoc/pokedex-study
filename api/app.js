d19ad8c30a6a7ec36f5bac0a50712fe1ea867be1
Meitoc <info@meitoc.com>
2023-09-28 15:26:56 +0700

    pokedex

--- api/app.js
+++ api/app.js
@@ -28,6 +28,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // app.use((req, res, next) => {
 //     res.setHeader(
 //       "Access-Control-Allow-Origin",
